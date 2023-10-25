452b251972448126cc2eb43a07a3eab33ebb0801
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-10-25 10:58:04 -0500

    commit_day25

--- routes/web.php
+++ routes/web.php
@@ -7,25 +7,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class, "show"]);
 
+// Lo que recibe este agrupador de controllers es la clase osea el controlador y el metodo group con una fucnion dentro todas las urls
+Route::controller(Cursos::class)->group(function(){
+    Route::get('Cursos/', 'show_cursos');
+});
 
-Route::get('Cursos/', [Cursos::class, 'show_cursos']);
 
 
-// Para mostrar todos los posts
-Route::post('Posts', [PostsController::class, 'store']);
 
-// Para mostrar un Post segun su ID
-Route::get('Posts/{id}', [PostsController::class, 'show']);
 
-// Para crear un Post
-Route::get('Posts/create', [PostsController::class, 'create']);
 
-// Para editar un post
-Route::get('Posts/{id}/edit', [PostsController::class, 'edit']);
 
-// Para guardar los cambios del edit del post edit
-Route::patch('Posts/{id}', [PostsController::class, 'update']);
+// ----------------------  A --------------------------------  //
 
-//Para Eliminar un post
-Route::delete('Posts/{id}', [PostsController::class, 'delete']);
-
+Route::controller(PostsController::class)->group(function(){
+    // Para mostrar todos los posts
+    Route::post('Posts', 'store');
+    
+    // Para mostrar un Post segun su ID
+    Route::get('/Posts/{id}', 'show');
+    
+    // Para crear un Post
+    Route::get('/Posts/create', 'create');
+    
+    // Para editar un post
+    Route::get('Posts/{id}/edit','edit');
+    
+    // Para guardar los cambios del edit del post edit
+    Route::patch('Posts/{id}', 'update');
+    
+    //Para Eliminar un post
+    Route::delete('Posts/{id}', 'delete');
+});    
