4db5c3269f8ff4c5e6e8ed0195d98d2248d02c1d
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-10-24 21:33:51 -0500

    a

--- resources/views/cursos.blade.php
+++ resources/views/cursos.blade.php
@@ -4,7 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mis cursos</title>
-    <script src="script/script.js"></script>
     @vite('resources/css/app.css')
 
 </head>
