024bbd26b17ceec62c9d75afce54fbe26a707fc1
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-10-26 13:18:45 -0500

    asdsa

---
+++ app/Http/Controllers/home_controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class home_controller extends Controller
+{
+    function show(){
+        return 'Hello world';
+    }
+}
