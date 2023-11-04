d1e744449702887157bf8d6a7f68ab8aab9de30b
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-11-04 10:11:52 -0500

    crudcheck

--- resources/views/Projects/edit.blade.php
+++ resources/views/Projects/edit.blade.php
@@ -1,14 +1,19 @@
-<form action="{{ route('Projecto', ['proyecto' => $project]) }}" method="POST">
-    @csrf
-    @method('PATCH')
-
-    <input type="text" name="title" placeholder="Titulo" value="{{$project->title}}">
-    <input type="text" name="description" placeholder="Descripcion" value="{{$project->description}}">
-    <select name="tecnologias">
-        <option value="{{$project->tecnologias}}" selected>{{$project->tecnologias}}</option>
-        <option value="Backend">Backend</option>
-        <option value="Frontend">Frontend</option>
-        <option value="Fullstack">Fullstack</option>
-    </select>
-    <input type="submit" value="Enviar">
-</form>
+@vite('resources/css/app.css')
+<body style="background-color: rgb(9, 9, 41)">
+    <div class="container mx-auto py-20 flex mt-5 justify-center bg-blue-800 rounded-lg max-w-max">
+        <form action="{{ route('Projecto', ['proyecto' => $project]) }}" method="POST" class="flex flex-col gap-4 p-10">
+            @csrf
+            @method('PATCH')
+        
+            <input class="p-2 rounded-lg" type="text" name="title" placeholder="Titulo" value="{{$project->title}}">
+            <input class="p-2 rounded-lg" type="text" name="description" placeholder="Descripcion" value="{{$project->description}}">
+            <select name="tecnologias">
+                <option value="{{$project->tecnologias}}" selected>{{$project->tecnologias}}</option>
+                <option value="Backend">Backend</option>
+                <option value="Frontend">Frontend</option>
+                <option value="Fullstack">Fullstack</option>
+            </select>
+            <input class="px-5 py-2 bg-black text-white rounded-lg" type="submit" value="Enviar">
+        </form>
+    </div>
+</body>
--- resources/views/Projects/new.blade.php
+++ resources/views/Projects/new.blade.php
@@ -1,11 +1,16 @@
-<form action="/Proyects/process" method="POST">
-    @csrf
-    <input type="text" name="title" placeholder="Titulo">
-    <input type="text" name="description" placeholder="Descripcion">
-    <select name="tecnologias">
-        <option value="Backend">Backend</option>
-        <option value="Frontend">Frontend</option>
-        <option value="Fullstack">Fullstack</option>
-    </select>
-    <input type="submit" value="Enviar">
-</form>
+@vite('resources/css/app.css')
+<body style="background-color: rgb(9, 9, 41)">
+    <div class="container mx-auto py-20 flex mt-5 justify-center bg-blue-800 rounded-lg max-w-max">
+        <form action="/Proyects/process" method="POST" class="flex flex-col gap-3 p-10">
+            @csrf
+            <input class="p-2 rounded-lg" type="text" name="title" placeholder="Titulo">
+            <input class="p-2 rounded-lg" type="text" name="description" placeholder="Descripcion">
+            <select name="tecnologias">
+                <option value="Backend">Backend</option>
+                <option value="Frontend">Frontend</option>
+                <option value="Fullstack">Fullstack</option>
+            </select>
+            <input class="px-5 py-2 bg-black text-white rounded-lg" type="submit" value="Enviar">
+        </form>
+    </div>
+</body>
