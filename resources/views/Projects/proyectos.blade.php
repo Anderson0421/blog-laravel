fc6b7b6061a1da9f3629985661d2413046a4d8e7
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-11-03 22:04:50 -0500

    crudpartr1

--- resources/views/Projects/proyectos.blade.php
+++ resources/views/Projects/proyectos.blade.php
@@ -4,8 +4,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Projects Page</title>
+    @vite('resources/css/app.css')
+
 </head>
-<body>
-    {{$projects->title}}
+<body style="background-color: rgb(9, 9, 41)">
+        <div class="container mx-auto pt-10 text-white">
+            <h1 class="text-2xl font-bold mb-2">Gestion de proyectos</h1>
+            <div>
+                <table class="table-auto text-white">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Descripcion</th>
+                            <th>Categoria</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                        <tr>
+                            <td>{{$project->id}}</td>
+                            <td>{{$project->title}}</td>
+                            <td>{{$project->description}}</td>
+                            <td>{{$project->tecnologias}}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
 </body>
 </html>
