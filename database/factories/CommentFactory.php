b03eb9aa991166579d5ef43bfe06990c60a8f84b
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-11-04 19:04:32 -0500

    a

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,11 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'content' => $this->faker->sentence(),
+            'post_id' => function(){
+                return Post::factory()->create();
+            },
         ];
     }
 }
